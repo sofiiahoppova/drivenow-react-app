9a7ac8142c2bd3572eb855abcbcaf555666a4c1b
Sofiia Hoppova <sofiahoppova@gmail.com>
2025-10-22 15:54:15 +0300

    Create BenefitsSection component and styles for it. Add content images

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroSection from "../components/HeroSection/HeroSection";
 import AboutUsSection from "../components/AboutUsSection/AboutUsSection";
+import BenefitsSection from "../components/BenefitsSection/BenefitsSection";
 
 const HomePage = () => {
   return (
     <div>
       <HeroSection />
       <AboutUsSection />
+      <BenefitsSection />
     </div>
   );
 };
