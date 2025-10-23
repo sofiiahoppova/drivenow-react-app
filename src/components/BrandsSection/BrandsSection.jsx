83619a29ab7f4313f44e8040910d6235e48dd979
Sofiia Hoppova <sofiahoppova@gmail.com>
2025-10-23 22:49:38 +0300

    Update BrandsSection component and add its styles

--- src/components/BrandsSection/BrandsSection.jsx
+++ src/components/BrandsSection/BrandsSection.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import css from "./BrandsSection.module.css";
-import { Navigation, Pagination, A11y } from "swiper/modules";
+import { Navigation, Pagination, A11y, EffectCoverflow } from "swiper/modules";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -9,57 +9,81 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
+import "swiper/css/effect-coverflow";
 
 const BrandsSection = () => {
   return (
-    <section>
-      <h2>Btands We Offer</h2>
-      <p>Select your favorite brand and discover available models.</p>
-      <Swiper
-        modules={[Navigation, Pagination, A11y]}
-        spaceBetween={50}
-        slidesPerView={3}
-        loop
-        navigation={{
-          nextEl: ".custom-next",
-          prevEl: ".custom-prev",
-        }}
-        pagination={{
-          el: ".custom-pagination",
-          clickable: true,
-        }}
-        onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log("slide change")}
-      >
-        <SwiperSlide>
-          <img src="/images/CarBrands/Audi.png" alt="Audi" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="/images/CarBrands/Toyota.png" alt="Toyota" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="/images/CarBrands/Volkswagen.png" alt="Volkswagen" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="/images/CarBrands/Nissan.png" alt="Nissan" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="/images/CarBrands/Mersedes.png" alt="Mercedes" />
-        </SwiperSlide>
-        <div className={css.controls}>
-          <div className="custom-prev">
-            <svg className={css.icon} width={20} height={32}>
-              <use href="/sprite.svg#icon-chevron"></use>
-            </svg>
-          </div>
-          <div className="custom-pagination"></div>
-          <div className="custom-next">
-            <svg className={css.icon} width={20} height={32}>
-              <use href="/sprite.svg#icon-chevron"></use>
-            </svg>
-          </div>
+    <section className={css.container}>
+      <h2 className={css.title}>Btands We Offer</h2>
+      <p className={css.description}>
+        Select your favorite brand and discover available models.
+      </p>
+      <div className={css.wrapper}>
+        <div className="custom-prev">
+          <svg className={css.icon} width={20} height={32}>
+            <use href="/sprite.svg#icon-chevron"></use>
+          </svg>
         </div>
-      </Swiper>
+        <Swiper
+          modules={[Navigation, Pagination, A11y, EffectCoverflow]}
+          //   effect="coverflow"
+          spaceBetween={50}
+          slidesPerView={3}
+          loop
+          navigation={{
+            nextEl: ".custom-next",
+            prevEl: ".custom-prev",
+          }}
+          pagination={{
+            el: ".custom-pagination",
+            clickable: true,
+          }}
+          onSwiper={(swiper) => console.log(swiper)}
+          onSlideChange={() => console.log("slide change")}
+        >
+          <SwiperSlide className={css.item}>
+            <img
+              className={css.image}
+              src="/images/CarBrands/Audi.png"
+              alt="Audi"
+            />
+          </SwiperSlide>
+          <SwiperSlide className={css.item}>
+            <img
+              className={css.image}
+              src="/images/CarBrands/Toyota.png"
+              alt="Toyota"
+            />
+          </SwiperSlide>
+          <SwiperSlide className={css.item}>
+            <img
+              className={css.image}
+              src="/images/CarBrands/Volkswagen.png"
+              alt="Volkswagen"
+            />
+          </SwiperSlide>
+          <SwiperSlide className={css.item}>
+            <img
+              className={css.image}
+              src="/images/CarBrands/Nissan.png"
+              alt="Nissan"
+            />
+          </SwiperSlide>
+          <SwiperSlide className={css.item}>
+            <img
+              className={css.image}
+              src="/images/CarBrands/Mersedes.png"
+              alt="Mercedes"
+            />
+          </SwiperSlide>
+        </Swiper>
+        <div className="custom-next">
+          <svg className={css.icon} width={20} height={32}>
+            <use href="/sprite.svg#icon-chevron"></use>
+          </svg>
+        </div>
+      </div>
+      <div className="custom-pagination"></div>
     </section>
   );
 };
