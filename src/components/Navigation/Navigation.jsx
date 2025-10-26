bcd098e04d19f81acfdfe4cffe37e39ed4db9c28
Sofiia Hoppova <sofiahoppova@gmail.com>
2025-10-26 19:52:34 +0200

    Move auth in, add close button and apply mobile menu logic

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,10 +4,9 @@ import clsx from "clsx";
 
 import css from "./Navigation.module.css";
 
-const Navigation = ({ color }) => {
+const Navigation = ({ color, onClose, isMobile }) => {
   const buildLinkClass = ({ isActive }) => {
     return clsx(
-      css.link,
       css.link,
       color === "black" && css.linkBlack,
       color === "white" && css.linkWhite,
@@ -16,30 +15,55 @@ const Navigation = ({ color }) => {
   };
 
   return (
-    <nav>
-      <ul className={css.navList}>
-        <li className={css.item}>
-          <NavLink to="/" className={buildLinkClass}>
-            Home
+    <div
+      className={clsx(
+        css.container,
+        isMobile && css.mobile,
+        !isMobile && css.hidden
+      )}
+      onClick={onClose}
+    >
+      <button className={css.closeButton} onClick={onClose}>
+        ✕
+      </button>
+      <nav>
+        <ul className={css.navList}>
+          <li className={css.item}>
+            <NavLink to="/" className={buildLinkClass}>
+              Home
+            </NavLink>
+          </li>
+          <li className={css.item}>
+            <NavLink to="/aboutus" className={buildLinkClass}>
+              AboutUs
+            </NavLink>
+          </li>
+          <li className={css.item}>
+            <NavLink to="/policies" className={buildLinkClass}>
+              Policies
+            </NavLink>
+          </li>
+          <li className={css.item}>
+            <NavLink to="/autopark" className={buildLinkClass}>
+              AutoPark
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      <ul className={css.auth}>
+        <li>
+          <NavLink to="/login" className={css.link}>
+            Login
           </NavLink>
         </li>
-        <li className={css.item}>
-          <NavLink to="/aboutus" className={buildLinkClass}>
-            AboutUs
-          </NavLink>
-        </li>
-        <li className={css.item}>
-          <NavLink to="/policies" className={buildLinkClass}>
-            Policies
-          </NavLink>
-        </li>
-        <li className={css.item}>
-          <NavLink to="/autopark" className={buildLinkClass}>
-            AutoPark
+        <li>|</li>
+        <li>
+          <NavLink to="/register" className={css.link}>
+            Register
           </NavLink>
         </li>
       </ul>
-    </nav>
+    </div>
   );
 };
 
