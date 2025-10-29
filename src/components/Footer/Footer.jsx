36cfb5fae39cf984039eedeac2755de564e3acf6
Sofiia Hoppova <sofiahoppova@gmail.com>
2025-10-29 18:22:06 +0200

    Remove Navigation from Footer. Add social links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,41 +1,47 @@
 import React from "react";
-import css from "./Footer.module.css";
+import clsx from "clsx";
+
 import Logo from "../Logo/Logo";
-import Navigation from "../Navigation/Navigation";
+
+import css from "./Footer.module.css";
 
 const Footer = () => {
   return (
     <footer className={css.container}>
-      <div className={css.mainWrapper}>
-        <div className={css.titleWrapper}>
-          <Logo color="white" />
-          <p className={css.description}>
-            DriveNow is where comfort meets style. Choose your car, hit the
-            road, and make every trip unforgettable.
-          </p>
-        </div>
-        <Navigation color="white" />
+      <div className={css.wrapper}>
+        <Logo color="white" />
+        <p className={css.description}>
+          DriveNow is where comfort meets style. Choose your car,
+          <br />
+          hit the road, and make every trip unforgettable.
+        </p>
       </div>
       <span className={css.line} />
       <div className={css.secondWrapper}>
-        <p className={css.description}>
+        <p className={clsx(css.description, css.leftAlign)}>
           &#169; 2025 DriveNow Car Rental. All rights reserved.
         </p>
         <ul className={css.iconsList}>
           <li>
-            <svg className={css.icon} width={24} height={24}>
-              <use href="/sprite.svg#icon-facebook"></use>
-            </svg>
+            <a href="https://www.instagram.com/" target="_blank">
+              <svg className={css.icon} width={24} height={24}>
+                <use href="/sprite.svg#icon-instagram"></use>
+              </svg>
+            </a>
           </li>
           <li>
-            <svg className={css.icon} width={24} height={24}>
-              <use href="/sprite.svg#icon-instagram"></use>
-            </svg>
+            <a href="https://www.facebook.com/" target="_blank">
+              <svg className={css.icon} width={24} height={24}>
+                <use href="/sprite.svg#icon-facebook"></use>
+              </svg>
+            </a>
           </li>
           <li>
-            <svg className={css.icon} width={24} height={24}>
-              <use href="/sprite.svg#icon-whatsapp"></use>
-            </svg>
+            <a href="https://www.whatsapp.com/" target="_blank">
+              <svg className={css.icon} width={24} height={24}>
+                <use href="/sprite.svg#icon-whatsapp"></use>
+              </svg>
+            </a>
           </li>
         </ul>
       </div>
