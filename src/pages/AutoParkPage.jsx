a8796007cb781fea36411b9fac2b8a6a9ec765e9
Sofiia Hoppova <sofiahoppova@gmail.com>
2025-11-01 18:51:01 +0200

    Add FiltersBar rendering to AutoPark page

--- src/pages/AutoParkPage.jsx
+++ src/pages/AutoParkPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import SearchBar from "../components/SearchBar/SearchBar";
 import CarsCatalog from "../components/CarsCatalog/CarsCatalog";
+import FiltersBar from "../components/FiltersBar/FiltersBar";
 
 const AutoParkPage = () => {
   return (
     <>
       <SearchBar />
+      <FiltersBar/>
       <CarsCatalog />
     </>
   );
