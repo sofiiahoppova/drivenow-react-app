f11211f3949db001dd9feda73e9dbc2485e85682
Sofiia Hoppova <sofiahoppova@gmail.com>
2025-11-02 18:33:15 +0200

    Implement pagination to CarsCatalog

--- src/components/CarsCatalog/CarsCatalog.jsx
+++ src/components/CarsCatalog/CarsCatalog.jsx
@@ -1,14 +1,19 @@
-import React from "react";
 import css from "./CarsCatalog.module.css";
+import React, { useState } from "react";
+
 import BasicCarCard from "../BasicCarCard/BasicCarCard";
+import Pagination from "./Pagination/Pagination";
 
 const CarsCatalog = () => {
   const cards = Array.from({ length: 8 });
+  const [currentPage, setCurrentPage] = useState(1);
+
   return (
     <section className={css.list}>
       {cards.map((_, index) => (
         <BasicCarCard key={index} />
       ))}
+      <Pagination currentPage={currentPage} setCurrentPage={setCurrentPage} />
     </section>
   );
 };
