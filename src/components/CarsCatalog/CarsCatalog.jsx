a293d5c5abaa7189a3418536176f840c12b67ed1
Sofiia Hoppova <sofiahoppova@gmail.com>
2025-11-02 18:59:25 +0200

    Pass state from CarsCatalog to CarCard

--- src/components/CarsCatalog/CarsCatalog.jsx
+++ src/components/CarsCatalog/CarsCatalog.jsx
@@ -1,18 +1,21 @@
-import css from "./CarsCatalog.module.css";
 import React, { useState } from "react";
 
 import BasicCarCard from "../BasicCarCard/BasicCarCard";
 import Pagination from "./Pagination/Pagination";
 
-const CarsCatalog = () => {
+import css from "./CarsCatalog.module.css";
+
+const CarsCatalog = ({ selectedDates }) => {
   const cards = Array.from({ length: 8 });
   const [currentPage, setCurrentPage] = useState(1);
 
   return (
-    <section className={css.list}>
-      {cards.map((_, index) => (
-        <BasicCarCard key={index} />
-      ))}
+    <section className={css.container}>
+      <div className={css.list}>
+        {cards.map((_, index) => (
+          <BasicCarCard key={index} selectedDates={selectedDates} />
+        ))}
+      </div>
       <Pagination currentPage={currentPage} setCurrentPage={setCurrentPage} />
     </section>
   );
