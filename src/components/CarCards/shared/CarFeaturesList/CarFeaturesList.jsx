02bd265d25fb7aeede07efe1bcff523acf3673bc
Sofiia Hoppova <sofiahoppova@gmail.com>
2025-11-07 14:07:15 +0200

    Apply dinamic size change logic to CarFeaturesList

--- src/components/CarCards/shared/CarFeaturesList/CarFeaturesList.jsx
+++ src/components/CarCards/shared/CarFeaturesList/CarFeaturesList.jsx
@@ -1,36 +1,39 @@
 import React from "react";
 
 import css from "./CarFeaturesList.module.css";
+import clsx from "clsx";
 
-const CarFeaturesList = ({ car }) => {
+const CarFeaturesList = ({ car, size = null }) => {
   return (
     <ul className={css.list}>
       <li className={css.listItem}>
-        <p className={css.caracteristic}>{car.year}</p>
+        <p className={clsx(size && css.small, css.text)}>{car.year}</p>
       </li>
       <li className={css.listItem}>
         <svg className={css.icon} width={24} height={24}>
           <use href="/sprite.svg#icon-seat"></use>
         </svg>
-        <p className={css.caracteristic}>{car.seats}</p>
+        <p className={clsx(size && css.small, css.text)}>{car.seats}</p>
       </li>
       <li className={css.listItem}>
         <svg className={css.icon} width={24} height={24}>
           <use href="/sprite.svg#icon-gas"></use>
         </svg>
-        <p className={css.caracteristic}>{car.fuel_type}</p>
+        <p className={clsx(size && css.small, css.text)}>{car.fuel_type}</p>
       </li>
       <li className={css.listItem}>
         <svg className={css.icon} width={24} height={24}>
           <use href="/sprite.svg#icon-AT"></use>
         </svg>
-        <p className={css.caracteristic}>{car.transmission}</p>
+        <p className={clsx(size && css.small, css.text)}>{car.transmission}</p>
       </li>
       <li className={css.listItem}>
         <svg className={css.icon} width={24} height={24}>
           <use href="/sprite.svg#icon-can"></use>
         </svg>
-        <p className={css.caracteristic}>{car.consumption_per_100km}l /100km</p>
+        <p className={clsx(size && css.small, css.text)}>
+          {car.consumption_per_100km}l /100km
+        </p>
       </li>
     </ul>
   );
