1b7cb2e5c0b35889f91431bec0101be25b7fdea2
Sofiia Hoppova <sofiahoppova@gmail.com>
2025-11-26 20:41:10 +0200

    Apply calculating funcs for price and days in BookingCard

--- src/components/CarCards/BookingCard/BookingCard.jsx
+++ src/components/CarCards/BookingCard/BookingCard.jsx
@@ -1,52 +1,24 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { useParams, useSearchParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 import clsx from "clsx";
-import toast from "react-hot-toast";
 
-import CarFeaturesList from "../shared/CarFeaturesList/CarFeaturesList";
+import { selectDates } from "../../../redux/dates/datesSlice";
+import { calculateDays } from "/src/utils/calculateDays";
+import { calculatePrice } from "../../../utils/calculatePrice";
 
 import css from "./BookingCard.module.css";
 
 import car from "../ExampleCarData.json";
 
-const BookingCard = ({ plan = "Full coverage", days = 4 }) => {
-  const [price, setPrice] = useState(0);
-  const [basicPrice, setBasicPrice] = useState(0);
-  const [coveragePrice, setCoveragePrice] = useState(0);
-  const [deposit, setDeposit] = useState(0);
+const BookingCard = () => {
+  const carId = useParams(); //to get the car by id
+  const [searchParams] = useSearchParams();
+  const plan = searchParams.get("plan");
 
-  useEffect(() => {
-    if (!car || !days) return;
-
-    let base = 0;
-    if (days <= 6) {
-      base = car.prices.dailyPrice * days;
-      setPrice(car.prices.dailyPrice);
-    } else if (days <= 13) {
-      base = car.prices.weekendPrice * days;
-      setPrice(car.prices.weekendPrice);
-    } else if (days <= 29) {
-      base = car.prices.weeklyPrice * days;
-      setPrice(car.prices.weeklyPrice);
-    } else if (days >= 30) {
-      base = car.prices.monthlyPrice * days;
-      setPrice(car.prices.monthlyPrice);
-    } else {
-      toast.error("Selected dates are invalid");
-      return;
-    }
-
-    setBasicPrice(base);
-
-    if (plan === "Full coverage") {
-      setCoveragePrice(base * 0.3);
-      setDeposit(0);
-    } else {
-      setCoveragePrice(0);
-      setDeposit(700);
-    }
-  }, [days, plan, car]);
-
-  const totalPrice = basicPrice + coveragePrice + deposit;
+  const [pickup, dropoff] = useSelector(selectDates);
+  const days = calculateDays(pickup, dropoff);
+  const price = calculatePrice(car, days, plan);
 
   return (
     <div className={css.container}>
@@ -65,37 +37,39 @@ const BookingCard = ({ plan = "Full coverage", days = 4 }) => {
           className={css.image}
         />
       </div>
-      {/* <CarFeaturesList car={car} size={"small"} /> */}
       <span className={css.divider} />
       <div className={css.priceWrapper}>
         <p className={css.text}>
-          {car.brand} {car.model} <span className={css.bold}>{price} $</span> x{" "}
+          {car.brand} {car.model}{" "}
+          <span className={css.bold}>{price.dailyRate} $</span> x{" "}
           <span className={css.bold}>{days} days</span>
         </p>
-        <p className={css.bold}>{basicPrice} $</p>
+        <p className={css.bold}>{price.base} $</p>
       </div>
       <span className={css.divider} />
 
       <div className={css.priceWrapper}>
-        <p className={css.text}>{plan}</p>
-        <span className={css.bold}>{coveragePrice} $</span>
+        <p className={css.text}>
+          {plan == "full" ? "Full covarage" : "Basic plan"}
+        </p>
+        <span className={css.bold}>{price.coveragePrice} $</span>
       </div>
       <div className={css.priceWrapper}>
         <p className={css.text}>Deposit</p>
         <span
           className={clsx(
             css.bold,
-            plan === "Full coverage" ? css.accentGreen : css.accentRed
+            plan === "full" ? css.accentGreen : css.accentRed
           )}
         >
-          {deposit} $
+          {price.deposit} $
         </span>
       </div>
 
       <span className={css.divider} />
       <div className={css.priceWrapper}>
         <p className={css.total}>Total</p>
-        <span className={css.total}>{totalPrice} $</span>
+        <span className={css.total}>{price.total} $</span>
       </div>
       <button className={css.bookBtn} type="submit">
         Confirm booking
