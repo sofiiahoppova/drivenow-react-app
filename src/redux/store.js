7ace0bda53e4a4f9350304044cfdee8373d18c6b
Sofiia Hoppova <sofiahoppova@gmail.com>
2025-11-26 20:48:07 +0200

    Add modal reducer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import datesReducer from "./dates/datesSlice.js";
+import modalReducer from "./modal/modalSlice.js";
 
 const store = configureStore({
   reducer: {
     dates: datesReducer,
+    modal: modalReducer,
   },
 });
 
