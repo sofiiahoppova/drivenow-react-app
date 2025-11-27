0fcc8898e2c4fa6fb3612fb50737cb223a90e1fe
Sofiia Hoppova <sofiahoppova@gmail.com>
2025-11-27 18:41:32 +0200

    Create SearchBarModal component as wrapper for SearchBar to use in modal

---
+++ src/components/SearchBar/SearchBarModal/SearchBarModal.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import toast from "react-hot-toast";
+
+import DatesInputs from "../DatesInputs/DatesInputs";
+
+import { datesAdded } from "../../../redux/dates/datesSlice";
+import { setClose } from "../../../redux/modal/modalSlice";
+
+import css from "./SearchBarModal.module.css";
+
+const SearchBarModal = ({ carId }) => {
+  const dispatch = useDispatch();
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const pickupDate = form.pickup.value;
+    const dropoffDate = form.dropoff.value;
+    const dateNow = Date.now();
+    const today = new Date(dateNow);
+
+    if (pickupDate >= dropoffDate) {
+      toast.error("Pick up date should be earlier than drop off date.");
+    } else if (pickupDate < today.toISOString().split("T")[0]) {
+      toast.error("Pick up date shouldn't be earliar than today");
+    } else {
+      dispatch(datesAdded([pickupDate, dropoffDate]));
+      toast.success("Dates selected successfully!");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSearch} className={css.form}>
+      <DatesInputs />
+      <Link to={`/booking/${carId}`} className={css.btnWrapper}>
+        <button
+          className={css.button}
+          type="submit"
+          onClick={() => dispatch(setClose())}
+        >
+          Book Now
+        </button>
+      </Link>
+    </form>
+  );
+};
+
+export default SearchBarModal;
