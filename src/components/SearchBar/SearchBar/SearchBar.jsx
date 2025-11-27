0db9ca008b5c39ca60775f13aea4b1b6109e3cf3
Sofiia Hoppova <sofiahoppova@gmail.com>
2025-11-27 18:38:40 +0200

    Refactor and restructure SearchBar, move inputs in separate component

--- src/components/SearchBar/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar/SearchBar.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import toast from "react-hot-toast";
 
+import DatesInputs from "../DatesInputs/DatesInputs";
+
+import { datesAdded } from "../../../redux/dates/datesSlice";
+
 import css from "./SearchBar.module.css";
-import { useDispatch, useSelector } from "react-redux";
-import { datesAdded, selectDates } from "../../redux/dates/datesSlice";
 
 const SearchBar = () => {
-  const dates = useSelector(selectDates);
   const dispatch = useDispatch();
 
   const handleSearch = (event) => {
@@ -36,28 +38,7 @@ const SearchBar = () => {
   return (
     <div className={css.container}>
       <form onSubmit={handleSearch} className={css.form}>
-        <div className={css.wrapper}>
-          <div className={css.inputWrapper}>
-            <label htmlFor="pickup-date">Pick-Up Date</label>
-            <input
-              className={css.dateInput}
-              type="date"
-              name="pickup"
-              id="pickup-date"
-              defaultValue={dates[0]}
-            />
-          </div>
-          <div className={css.inputWrapper}>
-            <label htmlFor="dropoff-date">Drop-Off Date</label>
-            <input
-              className={css.dateInput}
-              type="date"
-              name="dropoff"
-              id="dropoff-date"
-              defaultValue={dates[1]}
-            />
-          </div>
-        </div>
+        <DatesInputs />
         <button className={css.button} type="submit">
           <svg className={css.icon} width={20} height={20}>
             <use href="/sprite.svg#icon-search"></use>
