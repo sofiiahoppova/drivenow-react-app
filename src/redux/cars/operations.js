f5f4379b531f5e6a5c18395090e8f2563726f59b
Sofiia Hoppova <sofiahoppova@gmail.com>
2025-11-28 18:26:35 +0200

    Create carsSlice with operation and selectors

---
+++ src/redux/cars/operations.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+const BASE_URL = "https://drivenow-node-backend.onrender.com";
+
+const instance = axios.create({
+  baseURL: BASE_URL,
+});
+
+export const fetchAllCars = createAsyncThunk(
+  "cars/fetchAllCars",
+  async (filters, thunkAPI) => {
+    try {
+      const { data } = await instance.get("/cars", { params: { ...filters } });
+      console.log(data);
+      return data;
+    } catch (error) {
+      if (error.response) {
+        return thunkAPI.rejectWithValue(error.response.data);
+      }
+      return thunkAPI.rejectWithValue(error.message || "Unknown error");
+    }
+  }
+);
