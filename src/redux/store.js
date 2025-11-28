b11cbf24c4b6baae575265d8184e6ff11166ad6c
Sofiia Hoppova <sofiahoppova@gmail.com>
2025-11-28 18:29:25 +0200

    Add cars reducer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import datesReducer from "./dates/datesSlice.js";
 import modalReducer from "./modal/modalSlice.js";
+import carsReducer from "./cars/carsSlice.js";
 
 const store = configureStore({
   reducer: {
     dates: datesReducer,
     modal: modalReducer,
+    cars: carsReducer,
   },
 });
 
