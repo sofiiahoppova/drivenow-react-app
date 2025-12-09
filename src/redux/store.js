194f3c84d711541a0656bb1019c7b777cc42a8d3
Sofiia Hoppova <sofiahoppova@gmail.com>
2025-12-09 16:47:27 +0200

    Add refactored filtesr reducer to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import datesReducer from "./dates/datesSlice.js";
+import filtersReducer from "./filters/filtersSlice.js";
 import modalReducer from "./modal/modalSlice.js";
 import carsReducer from "./cars/carsSlice.js";
 
 const store = configureStore({
   reducer: {
-    dates: datesReducer,
+    filters: filtersReducer,
     modal: modalReducer,
     cars: carsReducer,
   },
