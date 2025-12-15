87752c65e51ab0f6dab1f94200e9d402c8b3f19f
Sofiia Hoppova <sofiahoppova@gmail.com>
2025-12-16 00:11:43 +0200

    Add and register auth and user reducers, implement persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,45 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import authReducer from "./auth/authSlice.js";
+import usersReducer from "./user/usersSlice.js";
+import carsReducer from "./cars/carsSlice.js";
 import filtersReducer from "./filters/filtersSlice.js";
 import modalReducer from "./modal/modalSlice.js";
-import carsReducer from "./cars/carsSlice.js";
 
-const store = configureStore({
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+// Persisting token field from auth slice to localstorage
+const authPersistConfig = {
+  key: "auth",
+  storage,
+  whitelist: ["token", "isAuthenticated"],
+};
+
+export const store = configureStore({
   reducer: {
+    auth: persistReducer(authPersistConfig, authReducer),
+    user: usersReducer,
+    cars: carsReducer,
     filters: filtersReducer,
     modal: modalReducer,
-    cars: carsReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  // devTools: process.env.NODE_ENV === 'production ',
 });
 
-export default store;
+export const persistor = persistStore(store);
