d2f1f4bb0fde1c693640d8dcc94d27ee45d1c612
Sofiia Hoppova <sofiahoppova@gmail.com>
2025-12-18 19:12:14 +0200

    Add and register bookings reducer to Redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,6 +3,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth/authSlice.js";
 import usersReducer from "./user/usersSlice.js";
 import carsReducer from "./cars/carsSlice.js";
+import bookingsReducer from "./bookings/bookingsSlice.js";
 import filtersReducer from "./filters/filtersSlice.js";
 import modalReducer from "./modal/modalSlice.js";
 
@@ -11,6 +12,7 @@ export const store = configureStore({
     auth: authReducer,
     user: usersReducer,
     cars: carsReducer,
+    bookings: bookingsReducer,
     filters: filtersReducer,
     modal: modalReducer,
   },
