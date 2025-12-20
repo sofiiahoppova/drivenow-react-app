6ef225fae80b15eb95c9cb596b980a3a355fde00
Sofiia Hoppova <sofiahoppova@gmail.com>
2025-12-20 17:26:10 +0200

    Add booking create logic using dispatch and refactor Formik schemas at BookingPage

--- src/pages/BookingPage.jsx
+++ src/pages/BookingPage.jsx
@@ -1,15 +1,22 @@
 import React from "react";
+import { useNavigate, useParams, useSearchParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import { Form, Formik } from "formik";
+import toast from "react-hot-toast";
 import * as Yup from "yup";
 
 import BookingCard from "../components/CarCards/BookingCard/BookingCard";
 import BookingForm from "../components/BookingForm/BookingForm";
 
+import { selectDates } from "../redux/filters/selectors";
+import { selectMe } from "../redux/user/selectors";
+import { createBooking } from "../redux/bookings/operations";
+
 import css from "./BookingPage.module.css";
 
-const BookingSchema = Yup.object().shape({
-  plan: Yup.string().oneOf(["basicPlan", "fullCoverage"]).required(),
-  paymentMethod: Yup.string().oneOf(["online", "offline"]).required(),
+const bookingSchema = Yup.object().shape({
+  plan: Yup.string().oneOf(["Basic plan", "Full coverage"]).required(),
+  paymentMethod: Yup.string().oneOf(["online", "offline"]).required("Required"),
 
   phoneNumber: Yup.string()
     .matches(/^\+?\d{10,15}$/, "Enter a valid phone number")
@@ -17,7 +24,7 @@ const BookingSchema = Yup.object().shape({
 
   dateOfBirth: Yup.string()
     .matches(
-      /^\d{4}\.\d{2}\.\d{2}$/,
+      /^\d{4}\-\d{2}\-\d{2}$/,
       "Date of birth must be in format YYYY.MM.DD"
     )
     .required("Required"),
@@ -33,25 +40,59 @@ const BookingSchema = Yup.object().shape({
   licensePhoto: Yup.mixed(),
 });
 
-const initialValues = {
-  plan: "basicPlan",
-  paymentMethod: "offline",
-
-  phoneNumber: "",
-  dateOfBirth: "",
-  fullName: "",
-  email: "",
-
-  passport: "",
-  passportPhoto: "",
-
-  license: "",
-  licensePhoto: "",
-};
-
 const BookingPage = () => {
-  const handleSubmit = (values) => {
-    console.log(values);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const user = useSelector(selectMe);
+
+  const initialValues = {
+    plan: "Basic plan",
+    paymentMethod: "",
+
+    fullName: user.fullName || "",
+    email: user.email || "",
+    phoneNumber: user.phoneNumber || "",
+    dateOfBirth: user.dateOfBirth ? user.dateOfBirth.split("T")[0] : "",
+
+    passport: user.passportSerial || "",
+    passportPhoto: "",
+
+    license: user.driverLicenseSerial || "",
+    licensePhoto: "",
+  };
+
+  const { id } = useParams();
+  const [searchParams] = useSearchParams();
+  const plan = searchParams.get("plan");
+
+  const dates = useSelector(selectDates);
+  const startDate = dates.startDate ?? localStorage.getItem("startDate");
+  const endDate = dates.endDate ?? localStorage.getItem("endDate");
+
+  const handleSubmit = async (values) => {
+    try {
+      await dispatch(
+        createBooking({
+          plan: plan === "basic" ? "basicPlan" : "fullCoverage",
+          carId: Number(id),
+          startDate,
+          endDate,
+          user: {
+            fullName: values.fullName,
+            phoneNumber: values.phoneNumber,
+            dateOfBirth: values.dateOfBirth,
+            email: values.email,
+            passportSerial: values.passport || null,
+            driverLicenseSerial: values.license || null,
+          },
+        })
+      ).unwrap();
+      toast.success("You have booked the car successfully!");
+      navigate("/account");
+    } catch (e) {
+      toast.error(e);
+    }
   };
 
   return (
@@ -59,7 +100,7 @@ const BookingPage = () => {
       <h2 className={css.title}>Booking Details & Verification</h2>
       <Formik
         initialValues={initialValues}
-        validationSchema={BookingSchema}
+        validationSchema={bookingSchema}
         onSubmit={handleSubmit}
       >
         <Form className={css.wrapper}>
