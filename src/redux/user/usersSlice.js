261fefe2703a4b64725a5f29ff7352f29f91a7cb
Sofiia Hoppova <sofiahoppova@gmail.com>
2025-12-20 17:31:42 +0200

    Refactor user Redux state: add auth fields, actions and selectors

--- src/redux/user/usersSlice.js
+++ src/redux/user/usersSlice.js
@@ -1,11 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { fetchUserMe, updateUserMe } from "./operations";
 
-const initialState = { user: null, status: "idle", error: null };
+const initialState = {
+  user: null,
+  status: "idle",
+  error: null,
+  isAuthenticated: false,
+  isInitialized: false,
+};
 
-export const carsSlice = createSlice({
+export const userSlice = createSlice({
   name: "user",
   initialState,
+  reducers: {
+    clearUser: () => initialState,
+    setAuth(state, action) {
+      state.isAuthenticated = action.payload;
+    },
+    setInit(state, action) {
+      state.isInitialized = action.payload;
+    },
+  },
   extraReducers: (builder) => {
     builder
       .addCase(fetchUserMe.pending, (state, action) => {
@@ -15,10 +30,14 @@ export const carsSlice = createSlice({
       .addCase(fetchUserMe.fulfilled, (state, action) => {
         state.status = "succeeded";
         state.user = action.payload.data;
+        state.isInitialized = true;
+        state.isAuthenticated = true;
       })
       .addCase(fetchUserMe.rejected, (state, action) => {
         state.status = "failed";
         state.error = action.error.message;
+        state.isAuthenticated = false;
+        state.isInitialized = true;
       })
       .addCase(updateUserMe.pending, (state, action) => {
         state.status = "loading";
@@ -26,6 +45,7 @@ export const carsSlice = createSlice({
       })
       .addCase(updateUserMe.fulfilled, (state, action) => {
         state.status = "succeeded";
+        state.user = action.payload.data;
       })
       .addCase(updateUserMe.rejected, (state, action) => {
         state.status = "failed";
@@ -34,4 +54,6 @@ export const carsSlice = createSlice({
   },
 });
 
-export default carsSlice.reducer;
+export const { clearUser, setAuth, setInit } = userSlice.actions;
+
+export default userSlice.reducer;
