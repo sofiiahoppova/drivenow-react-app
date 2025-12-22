a50584c9cab76676cc4e4090219a1920713cf881
Sofiia Hoppova <sofiahoppova@gmail.com>
2025-12-22 13:31:57 +0200

    Add new modal components to constants

--- src/constants/modalComponents.js
+++ src/constants/modalComponents.js
@@ -1,7 +1,13 @@
 import ReviewsList from "../components/ReviewsList/ReviewsList";
 import SearchBarModal from "../components/SearchBar/SearchBarModal/SearchBarModal";
+import LogOutModal from "../components/UserAccount/components/LogOutModal/LogOutModal";
+import AuthModal from "../components/Auth/components/AuthModal/AuthModal";
+import DeleteBookingModal from "../components/UserAccount/components/DeleteBookingModal/DeleteBookingModal";
 
 export const modalComponents = {
   ReviewsList,
   SearchBarModal,
+  LogOutModal,
+  AuthModal,
+  DeleteBookingModal,
 };
