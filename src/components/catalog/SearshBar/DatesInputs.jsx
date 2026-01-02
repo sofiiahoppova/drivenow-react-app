1a8b9435035c450845ba6e893f9ca7d60bee80ed
Sofiia Hoppova <sofiahoppova@gmail.com>
2026-01-02 13:51:51 +0200

    Refactor DatesInputs using Formik Fields, remove default values

--- src/components/catalog/SearshBar/DatesInputs.jsx
+++ src/components/catalog/SearshBar/DatesInputs.jsx
@@ -1,33 +1,27 @@
 import React from "react";
-import { useSelector } from "react-redux";
-
-import { selectDates } from "/src/redux/filters/selectors.js";
+import { Field } from "formik";
 
 import css from "./DatesInputs.module.css";
 
 const DatesInputs = () => {
-  const { startDate, endDate } = useSelector(selectDates);
-
   return (
     <div className={css.wrapper}>
       <div className={css.inputWrapper}>
         <label htmlFor="pickup-date">Pick-Up Date</label>
-        <input
+        <Field
           className={css.dateInput}
           type="date"
           name="pickup"
           id="pickup-date"
-          defaultValue={startDate}
         />
       </div>
       <div className={css.inputWrapper}>
         <label htmlFor="dropoff-date">Drop-Off Date</label>
-        <input
+        <Field
           className={css.dateInput}
           type="date"
           name="dropoff"
           id="dropoff-date"
-          defaultValue={endDate}
         />
       </div>
     </div>
