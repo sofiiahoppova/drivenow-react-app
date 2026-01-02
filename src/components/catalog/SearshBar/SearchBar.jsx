690ee7fc518ff51545e725a2a408b1bb5d6d4720
Sofiia Hoppova <sofiahoppova@gmail.com>
2026-01-02 13:50:14 +0200

    Refactor SearchBar basic form using Formik for more convenient usage

--- src/components/catalog/SearshBar/SearchBar.jsx
+++ src/components/catalog/SearshBar/SearchBar.jsx
@@ -1,21 +1,22 @@
 import React from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { Form, Formik } from "formik";
 import toast from "react-hot-toast";
 
 import DatesInputs from "./DatesInputs";
 
+import { selectDates } from "/src/redux/filters/selectors.js";
 import { setDates } from "/src/redux/filters/filtersSlice.js";
 
 import css from "./SearchBar.module.css";
 
 const SearchBar = () => {
   const dispatch = useDispatch();
+  const { startDate, endDate } = useSelector(selectDates);
 
-  const handleSearch = (event) => {
-    event.preventDefault();
-    const form = event.target;
-    const pickupDate = form.pickup.value;
-    const dropoffDate = form.dropoff.value;
+  const handleSearch = (values) => {
+    const pickupDate = values.pickup;
+    const dropoffDate = values.dropoff;
     const dateNow = Date.now();
     const today = new Date(dateNow);
 
@@ -37,17 +38,21 @@ const SearchBar = () => {
     }
   };
 
+  const initialValues = { pickup: startDate || "", dropoff: endDate || "" };
+
   return (
     <div className={css.container}>
-      <form onSubmit={handleSearch} className={css.form}>
-        <DatesInputs />
-        <button className={css.button} type="submit">
-          <svg className={css.icon} width={20} height={20}>
-            <use href="/sprite.svg#icon-search"></use>
-          </svg>
-          Search
-        </button>
-      </form>
+      <Formik initialValues={initialValues} onSubmit={handleSearch}>
+        <Form className={css.form}>
+          <DatesInputs />
+          <button type="submit" className={css.button}>
+            <svg className={css.icon} width={20} height={20}>
+              <use href="/sprite.svg#icon-search"></use>
+            </svg>
+            Search
+          </button>
+        </Form>
+      </Formik>
     </div>
   );
 };
